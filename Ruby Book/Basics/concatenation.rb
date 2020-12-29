29e2c5aac351acf1635969a6b3a9e06389eea993
Mohammed Ijairi <mohammedija@gmail.com>
2020-12-29 12:50:25 +0100

    Deleted the 'Basics' folder

--- Ruby Book/Basics/concatenation.rb
+++
@@ -1,7 +0,0 @@
-# Add two strings together that, when concatenated, return your first and last name as your full name in one string.
-
-first_name = 'Mohammed'
-last_name = 'Ijairi'
-
-puts 'Concatenation: ' + first_name + ' ' + last_name
-puts "Interpolation: #{first_name} #{last_name}"
